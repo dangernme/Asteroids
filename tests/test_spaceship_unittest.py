691f1d54cdc26a2c132981409aa2594b9d1a6169
dangernme <MichaelSarcevic@hotmail.de>
2024-10-03 23:24:21 +0200

    cleanup test file

--- tests/test_spaceship_unittest.py
+++ tests/test_spaceship_unittest.py
@@ -8,61 +8,61 @@ class TestSpaceship(unittest.TestCase):
     def setUp(self):
         pg.joystick.init()
         self.ship = Spaceship(Vec(settings.WIDTH //2,settings.HEIGHT //2))
-        
+
     def test_fire(self):
         self.ship.rockets = 5
         self.ship.fire()
         self.assertEqual(self.ship.rockets, 4)
         self.assertEqual(len(self.ship.active_rockets), 1)
-        
+
     def test_collition_left(self):
         self.ship.health = 10
         self.ship.direction = Vec(-1,0)
-        
+
         self.ship.pos.x = settings.TEXT_WIDTH - 10
         self.ship.pos.y = settings.HEIGHT // 2
-        
+
         self.ship.handle_border_collition()
-        
+
         self.assertEqual(self.ship.health, 9)
         self.assertEqual(self.ship.direction, Vec(1,0))
-        
+
     def test_collition_right(self):
         self.ship.health = 10
         self.ship.direction = Vec(1,0)
-        
+
         self.ship.pos.x = settings.WIDTH + 1
         self.ship.pos.y = settings.HEIGHT // 2
-        
+
         self.ship.handle_border_collition()
-        
+
         self.assertEqual(self.ship.health, 9)
         self.assertEqual(self.ship.direction, Vec(-1,0))
-        
+
     def test_collition_top(self):
         self.ship.health = 10
         self.ship.direction = Vec(0,-1)
-        
+
         self.ship.pos.x = settings.WIDTH // 2
         self.ship.pos.y = -1
-        
+
         self.ship.handle_border_collition()
-        
+
         self.assertEqual(self.ship.health, 9)
         self.assertEqual(self.ship.direction, Vec(0,1))
-        
+
     def test_collition_bottom(self):
         self.ship.health = 10
         self.ship.direction = Vec(0, 1)
-        
+
         self.ship.pos.x = settings.WIDTH // 2
         self.ship.pos.y = settings.HEIGHT +1
-        
+
         self.ship.handle_border_collition()
-        
+
         self.assertEqual(self.ship.health, 9)
         self.assertEqual(self.ship.direction, Vec(0, -1))
-        
+
     def test_ship_image_change(self):
         self.ship.health = 100
         self.assertEqual(self.ship.select_ship_img(), 0)
@@ -79,6 +79,6 @@ class TestSpaceship(unittest.TestCase):
         self.ship.health = 20
         self.assertEqual(self.ship.select_ship_img(), 3)
 
-        
+
 if __name__ == '__main__':
     unittest.main()
